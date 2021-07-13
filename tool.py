443579d29c1de9d027d905e26164906f93bb2557
thevioletstandard <76827395+thevioletstandard@users.noreply.github.com>
2021-07-12 21:01:23 -0400

    un votes

--- tool.py
+++ tool.py
@@ -1,6 +1,10 @@
+import bs4
 import praw
+import requests
 
 reddit = praw.Reddit("gpmods")
+
+year = int(input("Current in-game year: "))
 
 while True:
     action = int(input("0 to check credentials, 1 to post UN proposals, 2 to post UN voting, 3 to post UN results, 4 to exit: "))
@@ -11,7 +15,6 @@ while True:
         except:
             print("Credentials invalid")
     elif action == 1:
-        year = int(input("Current year: "))
         perm_unsc, temp_unsc = ["China", "France", "Russian Federation", "United Kingdom", "United States"], []
         while (member := input("Temporary UNSC members (e to exit): ")) != "e":
             temp_unsc.append(member)
@@ -23,7 +26,54 @@ while True:
         comment2.mod.distinguish(how="yes", sticky=False)
         print(post.url)
     elif action == 2:
-        raise NotImplementedError
+        link = input("Proposals link: ")
+        id = praw.models.Submission.id_from_url(link)
+        post = reddit.submission(id=id)
+        post.comments.replace_more()
+        claims = requests.get("https://www.reddit.com/r/GlobalPowers/wiki/list").text
+        claims = list(bs4.BeautifulSoup(claims, "html.parser").stripped_strings)
+        reso_unga, reso_unsc = [], []
+        if len(claims) < 100:
+            print("Reddit is broken, try again later")
+            continue
+        for comment in post.comments.list():
+            if comment.author.name == "GPMods" and "General Assembly" in comment.body:
+                for reply in comment.replies:
+                    if isinstance(reply, praw.models.Comment):
+                        try:
+                            if claims.index("/u/" + reply.author.name) != -1 and claims.index("/u/" + reply.author.name) < claims.index("Bougainville"): # standin for no recognition
+                                reso_unga.append(reply.body)
+                            else:
+                                reply.reply("You are not authorised to propose this resolution. If you believe this is a mistake, please contact the moderators.")
+                        except:
+                            reply.reply("You are not authorised to propose this resolution. If you believe this is a mistake, please contact the moderators.")
+            elif comment.author.name == "GPMods" and "Security Council" in comment.body:
+                for reply in comment.replies:
+                    if isinstance(reply, praw.models.Comment):
+                        try:
+                            if claims.index("/u/" + reply.author.name) != -1 and claims.index("/u/" + reply.author.name) < claims.index("Bougainville") and claims[claims.index("/u/" + reply.author.name) - 2] in post.selftext: # standin for no recognition and unsc
+                                reso_unsc.append(reply.body)
+                            else:
+                                reply.reply("You are not authorised to propose this resolution. If you believe this is a mistake, please contact the moderators.")
+                        except:
+                            reply.reply("You are not authorised to propose this resolution. If you believe this is a mistake, please contact the moderators.")
+        text = f"""This is the {year} United Nations voting thread. You can just comment "vote" under the "yes", "no", or "abstain" options. Only [UNSC members]({link}) may vote on the UNSC resolutions, and voting for new UNSC members will be done in a separate comment."""
+        post = reddit.subreddit("GlobalPowers").submit(f"[UN] United Nations {year} Voting", selftext=text)
+        comment1 = post.reply("**General Assembly Resolutions**")
+        comment1.mod.distinguish(how="yes", sticky=True)
+        comment2 = post.reply("**Security Council Resolutions**")
+        comment2.mod.distinguish(how="yes", sticky=False)
+        for i in range(len(reso_unga)):
+            reso = comment1.reply(f"**A/RES/{year - 1946}/{i + 1}**: {reso_unga[i]}")
+            reso.reply("Yes")
+            reso.reply("No")
+            reso.reply("Abstain")
+        for i in range(len(reso_unsc)):
+            reso = comment2.reply(f"**S/RES/{year - 1946}/{i + 1}**: {reso_unsc[i]}")
+            reso.reply("Yes")
+            reso.reply("No")
+            reso.reply("Abstain")
+        print(post.url)
     elif action == 3:
         raise NotImplementedError
     elif action == 4:
